5205a84c3b0f6aacbc04df9bf8f1b24977b659ed
_Isaac_30_ <64109546+isaacAlmanza@users.noreply.github.com>
2023-06-26 13:29:02 -0500

    add service auth.login add environments add function login form

--- src/app/modules/auth/components/login-form/login-form.component.ts
+++ src/app/modules/auth/components/login-form/login-form.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faPen, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '@services/auth.service';
+import { RequestStatus } from "@models/request-status.model";
+
 
 @Component({
   selector: 'app-login-form',
@@ -13,22 +16,34 @@ export class LoginFormComponent {
     email: ['', [Validators.email, Validators.required]],
     password: ['', [ Validators.required, Validators.minLength(6)]],
   });
+
   faPen = faPen;
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
-  status: string = 'init';
+  status: RequestStatus  = 'init';
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   doLogin() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email, password } = this.form.getRawValue();
-      // TODO
+      this.authService.login(email, password)
+      .subscribe({
+        next:()=>{
+          this.status ='success'
+          this.router.navigate(['/app'])
+        },
+        error:()=>{
+          this.status ='failed'
+          console.log("error login");
+        }
+      })
     } else {
       this.form.markAllAsTouched();
     }
